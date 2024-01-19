7c5c9c00c5b794379c2990d87841881db492f861
dodododddo <1452315956@qq.com>
2024-01-19 12:13:58 +0800

    完成第五章内容：逻辑、移位与空指令,已通过测试

--- openmips/openmips.srcs/sources_1/new/ex.v
+++ openmips/openmips.srcs/sources_1/new/ex.v
@@ -36,6 +36,7 @@ module ex(
 
 
     reg [`RegBus] logicout;
+    reg [`RegBus] shiftres;
 
     always @(*) begin
         if(rst == `RstEnable) begin
@@ -48,12 +49,54 @@ module ex(
                     logicout <= reg1_i | reg2_i;
                 end
 
+                `EXE_AND_OP: begin
+                    logicout <= reg1_i & reg2_i;
+                end
+
+                `EXE_XOR_OP: begin
+                    logicout <= reg1_i ^ reg2_i;
+                end
+
+                `EXE_NOR_OP: begin
+                    logicout <= ~(reg1_i | reg2_i);
+                end
+
                 default: begin
                     logicout <= `ZeroWord;
                 end
             endcase
         end
     end
+
+
+    always @(*) begin
+        if(rst == `RstEnable) begin
+            shiftres <= `ZeroWord;
+        end
+        
+        // 移位指令中reg1_i是shamt
+        else begin
+            case (aluop_i)
+                `EXE_SLL_OP: begin
+                    shiftres <= reg2_i << reg1_i[4:0];
+                end
+
+                `EXE_SRL_OP: begin
+                    shiftres <= reg2_i >> reg1_i[4:0];
+                end
+
+                `EXE_SRA_OP: begin
+                    shiftres <= ({32{reg2_i[31]}} << (6'd32 - {1'b0, reg1_i[4:0]})) | reg2_i >> reg1_i[4:0];
+                end
+
+                default: begin
+                    shiftres <= `ZeroWord;
+                end
+            endcase
+        end
+    end
+
+
 
     always @(*) begin
         wd_o <= wd_i;
@@ -63,6 +106,12 @@ module ex(
             `EXE_RES_LOGIC: begin
                 wdata_o <= logicout;
             end
+
+            `EXE_RES_SHIFT: begin
+                wdata_o <= shiftres;
+            end
+
+            
             default: begin
                 wdata_o <= `ZeroWord;
             end
